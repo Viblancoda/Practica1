178c38bcdbba3e1a8c30d23dc680034ca39726f1
Victor <vitukodb@gmail.com>
2023-10-24 14:13:31 +0100

    Jsoup

--- src/main/java/dacd/blanco/Main.java
+++ src/main/java/dacd/blanco/Main.java
@@ -1,19 +1,34 @@
 package dacd.blanco;
 
+import dacd.blanco.model.Product;
+import dacd.blanco.model.Supermarket;
+
+import javax.naming.NameNotFoundException;
+import javax.swing.text.html.StyleSheet;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        List<Supermarket> supermarkets = createSupermarkets();
+        List<Product> products = createProducts();
+        for (Supermarket s : supermarkets) {
+            fillSupermarket(s, products);
         }
+    }
+    private  static void fillSupermarket(Supermarket supermarket, List<Product> products){
+        products.forEach(p->supermarket.put(p, urlOf(supermarket,p)));
+    }
+    private static String urlOf(Supermarket s, Product p){
+        return null;
+    }
+    private static List<Supermarket> createSupermarkets(){
+        //TODO
+        return List.of();
+        }
+    public static List<Product> createProducts(){
+        //TODO
+        return List.of();
     }
 }
--- src/main/java/dacd/blanco/model/Supermarket.java
+++ src/main/java/dacd/blanco/model/Supermarket.java
@@ -1,2 +1,21 @@
-package dacd.blanco.model;public class Supermarket {
+package dacd.blanco.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Supermarket {
+    private final String name;
+    private final Map<Product, Float> prices;
+    private final Map<Product, String> sources;
+
+    public Supermarket(String name) {
+        this.name = name;
+        this.prices = new HashMap<>();
+        this.sources = new HashMap<>();
+    }
+    public void put(Product product, String url){
+        sources.put(product, url);
+        prices.putIfAbsent(product, Float.NaN);
+    }
+
 }
